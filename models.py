7445970c5ceb75a49b3f0268da58a51afec3d70d
Beckett Dunlavy <bdunlavy@Becketts-Mac-mini.local>
2026-02-13 23:22:39 -0700

    finals touches

--- models.py
+++ models.py
@@ -59,6 +59,61 @@ class LogisticRegressionGD:
             loss = (-y.dot(np.log(output)) - (1 - y).dot(np.log(1 - output))) / X.shape[0]
             self.losses_.append(loss)
         return self
+    
+    def fit_sgd(self, X, y):
+        X_b = self._add_bias(X)
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X_b.shape[1])
+        self.losses_ = []
+
+        for i in range(self.n_iter):
+            indices = rgen.permutation(X_b.shape[0])
+            X_shuffled = X_b[indices]
+            y_shuffled = y[indices]
+
+            for xi, yi in zip(X_shuffled, y_shuffled):
+                xi = xi.reshape(1, -1)
+                net_input = xi.dot(self.w_)
+                output = self.activation(net_input)
+                error = yi - output
+                self.w_ += self.eta * 2.0 * xi.T.dot(error)
+
+            # Compute loss on data
+            net_input_all = X_b.dot(self.w_)
+            output_all = self.activation(net_input_all)
+            loss = (-y.dot(np.log(output_all)) - (1 - y).dot(np.log(1 - output_all))) / X.shape[0]
+            self.losses_.append(loss)
+        return self
+    
+    def fit_mini_batch_sgd(self, X, y, batch_size=32):
+       
+        X_b = self._add_bias(X)
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X_b.shape[1])
+        self.losses_ = []
+        n_samples = X_b.shape[0]
+
+        for i in range(self.n_iter):
+            indices = rgen.permutation(n_samples)
+            X_shuffled = X_b[indices]
+            y_shuffled = y[indices]
+
+            for start in range(0, n_samples, batch_size):
+                end = min(start + batch_size, n_samples)
+                X_batch = X_shuffled[start:end]
+                y_batch = y_shuffled[start:end]
+
+                net_input = X_batch.dot(self.w_)
+                output = self.activation(net_input)
+                errors = y_batch - output
+                self.w_ += self.eta * 2.0 * X_batch.T.dot(errors) / X_batch.shape[0]
+
+            # Compute loss 
+            net_input_all = X_b.dot(self.w_)
+            output_all = self.activation(net_input_all)
+            loss = (-y.dot(np.log(output_all)) - (1 - y).dot(np.log(1 - output_all))) / X.shape[0]
+            self.losses_.append(loss)
+        return self
 
     def net_input(self, X):
         X_b = self._add_bias(X)
